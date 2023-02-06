67bfb08b4582d0c5f6825ba0012d218ff35f76ad
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2023-02-06 09:29:58 +0100

    prova log

--- js/gesture-handler.js
+++ js/gesture-handler.js
@@ -1,69 +1,68 @@
 /* global AFRAME, THREE */
-
-AFRAME.registerComponent("gesture-handler", {
-    schema: {
-      enabled: { default: true },
-      rotationFactor: { default: 5 },
-      minScale: { default: 0.3 },
-      maxScale: { default: 10 },
-    },
-  
-    init: function () {
-      this.handleScale = this.handleScale.bind(this);
-      this.handleRotation = this.handleRotation.bind(this);
-  
-      this.isVisible = false;
-      this.initialScale = this.el.object3D.scale.clone();
-      this.scaleFactor = 1;
-  
-      this.el.sceneEl.addEventListener("markerFound", (e) => {
-        this.isVisible = true;
-      });
-  
-      this.el.sceneEl.addEventListener("markerLost", (e) => {
+function addGesture(){
+  AFRAME.registerComponent("gesture-handler", {
+      schema: {
+        enabled: { default: true },
+        rotationFactor: { default: 5 },
+        minScale: { default: 0.3 },
+        maxScale: { default: 10 },
+      },
+    
+      init: function () {
+        this.handleScale = this.handleScale.bind(this);
+        this.handleRotation = this.handleRotation.bind(this);
+    
         this.isVisible = false;
-      });      
-    },
-  
-    update: function () {
-      console.log('old');
-      if (this.data.enabled) {
-        this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
-        this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
-      } else {
+        this.initialScale = this.el.object3D.scale.clone();
+        this.scaleFactor = 1;
+    
+        this.el.sceneEl.addEventListener("markerFound", (e) => {
+          this.isVisible = true;
+        });
+    
+        this.el.sceneEl.addEventListener("markerLost", (e) => {
+          this.isVisible = false;
+        });
+      },
+    
+      update: function () {
+        console.log('old');
+        if (this.data.enabled) {
+          this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
+          this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
+        } else {
+          this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+          this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
+        }
+        //this.initialScale = this.el.object3D.scale.clone();
+      },
+    
+      remove: function () {
         this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
         this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
-      }
-      //this.initialScale = this.el.object3D.scale.clone();
-      console.log('u:'+this.el.object3D.scale.x);
-    },
-  
-    remove: function () {
-      this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
-      this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
-    },
-  
-    handleRotation: function (event) {
-      if (this.isVisible) {
-        this.el.object3D.rotation.y += event.detail.positionChange.x * this.data.rotationFactor;
-        this.el.object3D.rotation.x += event.detail.positionChange.y * this.data.rotationFactor;
-      }
-      console.log('p'+this.el.object3D.scale.x);
-    },
-  
-    handleScale: function (event) {
-      if (this.isVisible) {
-        this.scaleFactor *=
-          1 + event.detail.spreadChange / event.detail.startSpread;
-  
-        this.scaleFactor = Math.min(
-          Math.max(this.scaleFactor, this.data.minScale),
-          this.data.maxScale
-        );
-  
-        this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-        this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-        this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
-      }
-    },
-  });
+      },
+    
+      handleRotation: function (event) {
+        if (this.isVisible) {
+          this.el.object3D.rotation.y += event.detail.positionChange.x * this.data.rotationFactor;
+          this.el.object3D.rotation.x += event.detail.positionChange.y * this.data.rotationFactor;
+        }
+      },
+    
+      handleScale: function (event) {
+        if (this.isVisible) {
+          this.scaleFactor *=
+            1 + event.detail.spreadChange / event.detail.startSpread;
+    
+          this.scaleFactor = Math.min(
+            Math.max(this.scaleFactor, this.data.minScale),
+            this.data.maxScale
+          );
+    
+          this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
+          this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
+          this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
+        }
+      },
+    });
+}
