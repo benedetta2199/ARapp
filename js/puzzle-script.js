90ac1af7c5b3e523ad6efaf2d488250fbf518660
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2023-06-10 10:25:12 +0200

    dimensioni puzzle

--- js/puzzle-script.js
+++ js/puzzle-script.js
@@ -25,7 +25,7 @@ const app = new Vue({
   mounted() {
     // store a reference to vue for inside the ZIM Frame
     const vue = this;
-    const frame = new Frame("fit", 750, 1000, '#00000000', '#00000000', srcImg,);
+    const frame = new Frame("fit", 825, 1100, '#00000000', '#00000000', srcImg,);
       frame.on("ready", () => {// ES6 Arrow Function - similar to function(){} {ready, assets:"SpecialElite-Regular.ttf", path:"/assets/font"}
         //zog("ready from ZIM Frame"); // logs in console (F12 - choose console)
         frame.loadAssets({font: "Elite", src:"./asset/font/SpecialElite-Regular.ttf"});
